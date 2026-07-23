e6a28cfbaf5399e915bba765a8bfd5ae7b1314e8
mujahid <mujahidhussen75@gmail.com>
2026-07-23 14:42:21 +0300

    mmmmmmmm

--- fix_sysadmin.sql
+++
@@ -1,5 +0,0 @@
--- Set default admin password: Admin@Rammis2025!
--- bcrypt hash (rounds=12)
-UPDATE users
-SET password_hash = '$2a$12$g4Uf7j4fi4atlpbKnlJoheZAOVpGwk/9pZfZKCboEQjX2OT3IBCYK'
-WHERE username = 'sysadmin';
